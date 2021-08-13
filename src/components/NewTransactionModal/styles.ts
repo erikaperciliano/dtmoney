a1bcb8e919c38726a0e02157e4df60d5a7e2d1c8
Erika <erikaperciliano.developer@gmail.com>
2021-08-13 20:50:19 -0300

    applying validation on Styled Component

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { darken } from 'polished';
+import { darken, transparentize } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -63,6 +63,12 @@ export const TrasactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
@@ -70,7 +76,10 @@ export const RadioBox = styled.button<RadioBoxProps>`
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: ${(props) => props.isActive ? '#ccc' : 'tranparent'};
+        background: ${(props) => props.isActive 
+            ? transparentize(0.9, colors[props.activeColor]) 
+            : 'tranparent'
+        };
 
         display: flex;
         align-items: center;
