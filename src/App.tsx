c8690906800d05e44f14daac5c855f542f4b68ed
Erika <erikaperciliano.developer@gmail.com>
2021-08-16 19:59:36 -0300

    applying list transactions and seeds

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,6 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
-
       <Dashboard/>
       <NewTransactionModal 
         isOpen={isNewTransactionModalOpen}
