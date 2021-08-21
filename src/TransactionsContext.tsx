c44df3bb0cb9614d799f9e80d84e957e855c6238
Erika <erikaperciliano.developer@gmail.com>
2021-08-20 22:46:32 -0300

    moving create to context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -15,7 +15,17 @@ interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]); 
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'> // herda todos os campos do Transaction menos o id e o createdAt
+// type TransactionInput = Pick<Transaction, 'title'| 'amount' | 'type' | 'category'>   // selectiona quais campos desejados
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+); 
 
 export  function TransactionsProvider({ children }: TransactionsProviderProps){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -25,8 +35,12 @@ export  function TransactionsProvider({ children }: TransactionsProviderProps){
         .then(response => setTransactions(response.data.transactions))
     }, []);
 
+    function createTransaction(transaction: TransactionInput){
+        api.post('/transactions', transaction);
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             { children }
         </TransactionsContext.Provider>
     )
